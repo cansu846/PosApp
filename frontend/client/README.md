2b0637339c3dddfe90531e4bc106eaff838c8eb7
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-12-01 23:21:39 +0300

    feat: added client

---
+++ frontend/client/README.md
@@ -0,0 +1,32 @@
+ flex: yatay ve dikey hizalarda nasıl görüneceğini, öğelerin kendi içinde hizalanmalarını ve sırasını belirlemek için kullanılır.Tüm çözünürlüklerde ve cihazlarda daha hızlı ve esnek bir yapı sunar.
+
+Flex genel anlamıyla bir CSS özelliğinin adı değil, yapının adıdır. Dolayısı ile flex yapısı altında hem kapsayıcı (container) için hemde içindeki öğeler (items) için birden fazla flex özelliği bulunmaktadır. 
+
+
+max-w özelliği, öğenin genişliğinin alabileceği en büyük değeri belirler. Yani:
+
+Bir öğe, içeriğine veya konteynerine göre genişleyebilir, ancak max-w ile belirtilen değeri aşamaz.
+
+gap: 10px; /* veya **row-gap ve column-gap kullanarak 
+yatay ve dikey boşlukları kontrol edebilirsiniz** */
+flexbox, grid ve multi-column yapılarda esnek öğeler arasındaki boşluğu ayarlamak için kullanılmaktadır.
+
+justify-content
+Öğelerin yatay eksende hizasını ayarlamak için kullanılır.
+
+align-items
+Öğelerin dikey eksende hizasını ayarlamak için kullanılır
+
+flex-grow
+Bir öğeyi diğerlerine göre daha büyük göstermek için kullanılır. 
+
+flex-shrink
+Bu aslında flex-grow özelliğinin tam aksine, küçültme işlemi için kullanılır. 
+
+flex-basis
+Öğelerin eğer alanı yetiyorsa olması gereken genişliği belirlemek için kullanılır.
+
+flex
+Sırasıyla flex-grow, flex-shrink ve flex-basis özelliklerinin kısa kullanımıdır. İlk değeri zorunlu olup (shrink ve basis) değerlerini belirtmek zorunlu değildir. Varsayılan olarak 0 1 auto değerine sahiptir
+
+Offset terimi, bir öğenin bir diğerine veya konteynerine göre konumlandırılmasını ifade eder. Offset, genellikle öğenin yerleşim konumunu belirlemek için kullanılan değerlerdir. Bu, özellikle CSS positioning (konumlandırma) özellikleriyle birlikte kullanılır.
---
+++ frontend/client/src/components/header/Header.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Input, Badge } from 'antd';
+import { SearchOutlined, 
+    HomeOutlined, 
+    ShoppingCartOutlined,
+    UserOutlined,
+    BarChartOutlined,
+    LogoutOutlined,
+    CopyOutlined
+} from '@ant-design/icons';
+
+// rfce
+function Header() {
+  return (
+    <div className='border-b mb-6'>
+       <header className='py-3 ml-5 mr-5 flex items-center justify-between gap-10'>
+       <div className='logo'>
+        <a href=''>
+            <h2 className='text-2xl font-bold md:text-4xl'>LOGO</h2>
+        </a>
+       </div>
+        <div className='header-search flex-1  flex justify-center'>
+        <Input size="large" placeholder="Search..." className="rounded-full max-w-[800px]" prefix={<SearchOutlined />} />
+
+        </div>
+        <div className='menu-links flex justify-between items-center gap-8
+         md:static fixed bottom-0 md:w-auto w-screen md:bg-transparent
+          bg-white md:border-t-0 border-t md:px-0 px-10 md:pt-0 pt-3'>
+           <a href="/" className='flex flex-col hover:text-[#40a9ff]'>
+           <span><HomeOutlined className='md:text-2xl text-xl'/></span>
+           <span className='md:text-xs text-[10px]'>Home</span>
+           </a>
+           
+           {/* Buyuk ekranlarda flex iken küçük ekranlarda gizlenir */}
+           <a href="/" className='menu-link md:flex hidden flex-col hover:text-[#40a9ff] '>
+           <Badge count={5}> 
+           <span><ShoppingCartOutlined className='md:text-2xl text-xl'/></span>
+           </Badge>
+           
+           <span className='md:text-xs text-[10px]'>Basket</span>
+           </a>
+
+           <a href="/" className='flex flex-col hover:text-[#40a9ff]'>
+           <span><CopyOutlined className='md:text-2xl text-xl'/></span>
+           <span className='md:text-xs text-[10px]'>Invoice</span>
+           </a>
+         
+           <a href="/" className='flex flex-col hover:text-[#40a9ff]'>
+           <span><UserOutlined className='md:text-2xl text-xl'/></span>
+           <span className='md:text-xs text-[10px]'>User</span>
+           </a>
+
+           
+           <a href="/" className='flex flex-col hover:text-[#40a9ff]'>
+           <span><BarChartOutlined className='md:text-2xl text-xl'/></span>
+           <span className='md:text-xs text-[10px]'>Statistic</span>
+           </a>
+       
+           <a href="/" className='flex flex-col hover:text-[#40a9ff]'>
+           <span><LogoutOutlined className='md:text-2xl text-xl'/></span>
+           <span className='md:text-xs text-[10px]'>Exit</span>
+           </a>
+        
+        
+        </div>
+
+        {/* menu links ten farklı olarak fazladan eklendi ekran küçüldüğünde ortaya çıkar */}
+        <a href="/" className='menu-link flex flex-col hover:text-[#40a9ff] md:hidden'>
+           <Badge count={5}> 
+           <span><ShoppingCartOutlined className='md:text-2xl text-xl'/></span>
+           </Badge>
+           
+           <span className='md:text-xs text-[10px]'>Basket</span>
+           </a>
+       </header>
+    </div>
+    
+  )
+}
+
+export default Header
