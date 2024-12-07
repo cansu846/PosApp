e961e6aee631240e6e8056365018e6cabe392b7a
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-12-07 22:43:17 +0300

    feat: added register

--- frontend/client/src/pages/auth/Register.jsx
+++ frontend/client/src/pages/auth/Register.jsx
@@ -1,12 +1,41 @@
-import { Button, Form, Input } from "antd";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Button, Form, Input, message } from "antd";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Carousel } from 'antd';
 
 import AuthCarousel from '../../components/auth/AuthCarousel';
 
 
 function Register() {
+
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+
+    const onFinish = async (values) => {
+        setLoading(true);
+        try {
+            console.log(values);
+            const response = await fetch("http://localhost:5000/api/auth/register", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            });
+            //console.log(response);
+            if(response.status===200){
+                message.success("User save successfully.");
+                setLoading(false);
+                navigate("/login");
+            }
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+
+    }
+
     return (
         <div className="h-screen">
             <div className="flex justify-between h-full">
@@ -15,6 +44,7 @@ function Register() {
                     <div className="">
                         <Form
                             layout={"vertical"}
+                            onFinish={onFinish}
                         >
                             <Form.Item
                                 label="User Name"
@@ -64,22 +94,22 @@ function Register() {
                                     },
                                     ({ getFieldValue }) => ({
                                         validator(_, value) {
-                                          if (!value || getFieldValue("password") === value) {
-                                            return Promise.resolve();
-                                          }
-                                          return Promise.reject(
-                                            new Error(
-                                              "Passwords must be same!"
-                                            )
-                                          );
+                                            if (!value || getFieldValue("password") === value) {
+                                                return Promise.resolve();
+                                            }
+                                            return Promise.reject(
+                                                new Error(
+                                                    "Passwords must be same!"
+                                                )
+                                            );
                                         },
-                                      }),
+                                    }),
                                 ]}
                             >
                                 <Input.Password />
                             </Form.Item>
                             <Form.Item>
-                                <Button type="primary" htmlType="submit" size="large" className=" w-full">Sign Up</Button>
+                                <Button type="primary" htmlType="submit" loading={loading} size="large" className=" w-full">Sign Up</Button>
                             </Form.Item>
                         </Form>
 
@@ -95,33 +125,33 @@ function Register() {
                 </div>
                 {/* display: flex; Flexbox düzenini etkinleştirir. asagıdaki css için flex kullanılmazsa md de görünmüyor*/}
                 <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff] h-full">
-          <div className="w-full h-full flex items-center">
-            <div className="w-full">
-              <Carousel className="!h-full px-6">
-                <AuthCarousel
-                  img="/images/responsive.svg"
-                  title="Responsive"
-                  desc="Tüm Cihaz Boyutlarıyla Uyumluluk"
-                />
-                <AuthCarousel
-                  img="/images/statistic.svg"
-                  title="İstatistikler"
-                  desc="Geniş Tutulan İstatistikler"
-                />
-                <AuthCarousel
-                  img="/images/customer.svg"
-                  title="Müşteri Memnuniyeti"
-                  desc="Deneyim Sonunda Üründen Memnun Müşteriler"
-                />
-                <AuthCarousel
-                  img="/images/admin.svg"
-                  title="Yönetici Paneli"
-                  desc="Tek Yerden Yönetim"
-                />
-              </Carousel>
-            </div>
-          </div>
-        </div>
+                    <div className="w-full h-full flex items-center">
+                        <div className="w-full">
+                            <Carousel className="!h-full px-6">
+                                <AuthCarousel
+                                    img="/images/responsive.svg"
+                                    title="Responsive"
+                                    desc="Tüm Cihaz Boyutlarıyla Uyumluluk"
+                                />
+                                <AuthCarousel
+                                    img="/images/statistic.svg"
+                                    title="İstatistikler"
+                                    desc="Geniş Tutulan İstatistikler"
+                                />
+                                <AuthCarousel
+                                    img="/images/customer.svg"
+                                    title="Müşteri Memnuniyeti"
+                                    desc="Deneyim Sonunda Üründen Memnun Müşteriler"
+                                />
+                                <AuthCarousel
+                                    img="/images/admin.svg"
+                                    title="Yönetici Paneli"
+                                    desc="Tek Yerden Yönetim"
+                                />
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
